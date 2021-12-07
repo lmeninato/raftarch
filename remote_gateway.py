618ae35109379118a9d57618d743f1518e0857df
Lorenzo Meninato <lorenzomeninato@gmail.com>
2021-12-07 15:07:39 -0500

    Fix bug in remote gateway

--- remote_gateway.py
+++ remote_gateway.py
@@ -6,7 +6,7 @@ from lvcloud.gateway.gateway import Gateway
 logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
                     datefmt='%Y-%m-%d:%H:%M:%S', level=logging.INFO)
 
-clusters = [['10.10.1.1:5001', '10.10.1.2:5001' '10.10.1.3:5001']]
+clusters = [['10.10.1.1:5001', '10.10.1.2:5001', '10.10.1.3:5001']]
 
 gateway = Gateway(clusters, port = 8000)
 gateway.run()
