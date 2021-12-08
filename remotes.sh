c195a898ddc8366824f56bf0456a17182a1bfb75
Lorenzo Meninato <lorenzomeninato@gmail.com>
2021-12-08 15:14:06 -0500

    Add benchmarks for txns

--- remotes.sh
+++ remotes.sh
@@ -62,6 +62,10 @@ ssh va2083@apt184.apt.emulab.net "cd raftarch && git pull origin master"
 
 ssh va2083@apt175.apt.emulab.net "python3 raftarch/remote_gateway.py 2"
 
+# for killing
+ssh va2083@apt166.apt.emulab.net "pkill python3"
+
+
 ssh va2083@apt166.apt.emulab.net "python3 raftarch/launch_db_node.py http://10.10.1.4:8000 10.10.1.1:5001 10.10.1.2:5001 10.10.1.3:5001 &"
 ssh va2083@apt187.apt.emulab.net "python3 raftarch/launch_db_node.py http://10.10.1.4:8000 10.10.1.2:5001 10.10.1.1:5001 10.10.1.3:5001 &"
 ssh va2083@apt184.apt.emulab.net "python3 raftarch/launch_db_node.py http://10.10.1.4:8000 10.10.1.3:5001 10.10.1.1:5001 10.10.1.2:5001 &"
@@ -72,9 +76,9 @@ ssh va2083@apt184.apt.emulab.net "python3 raftarch/launch_db_node.py http://10.1
 
 # benchmark no failures
 ssh va2083@apt175.apt.emulab.net "python3 raftarch/no_failures.py"
-scp va2083@apt175.apt.emulab.net:~/no_failure.csv benchmarks_remote/with_lock_no_failure_2_clusters.csv
+scp va2083@apt175.apt.emulab.net:~/no_failure.csv benchmarks_remote/with_txn_no_failure_2_clusters.csv
 
 # benchmark with failures (including leader) at 10s
 ssh va2083@apt175.apt.emulab.net "python3 raftarch/leader_failure.py"
-scp va2083@apt175.apt.emulab.net:~/results_leader_failure.csv benchmarks_remote/with_lock_leader_failure_2_clusters.csv
+scp va2083@apt175.apt.emulab.net:~/results_leader_failure.csv benchmarks_remote/with_txn_leader_failure_2_clusters.csv
 
